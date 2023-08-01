97ae38cde3dfa718b2a97625f32733799dbb4372
simo.zgaoua <m.zgaoua2001@gmail.com>
2023-08-01 18:09:38 +0100

    first commit

---
+++ Desktop/my-portfolio/src/App.js
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <About />
+    <Skills />
+    </>
+   
+  );
+}
+
+export default App;
